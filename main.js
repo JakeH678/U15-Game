714fa97aec302fc729819c48c5a2bd9741ef7a61
JakeH678 <S0036442@wyke.ac.uk>
2025-03-27 10:53:58 +0000

    added basic enemies that are cheese

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bg;
 let player;
-
+let enemy;
+let en;
 function movement(){
     if (kb.pressing("shift")){
         if (kb.pressing("A")){
@@ -58,10 +59,30 @@ player = new Sprite(windowWidth/2,windowHeight-130,10,10,"d");
 player.img = "assets/car.png"
 player.scale = (windowWidth/800)/8
 player.rotation=270
+player.width = 120
+player.height = 120
+player.debug = true
+
+
+enemy = new Group();
+enemy.img = "assets/enemy.png"
+enemy.scale =0.025
 }
 
 
 function draw(){
 background(bg)
 movement()
+enemymovement()
+    while (enemy.length<enemynum){
+        let en = new enemy.Sprite(random(0,windowWidth),0,10,10,"d")
+    }
+}
+function enemymovement(){
+    for(e of enemy){
+        e.vel.y=1.3
+        if (e.y>windowHeight){
+            e.remove()
+        }
+    }
 }
