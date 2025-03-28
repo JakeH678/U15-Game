29954096cc7557605e2c00495bb4001675bf6824
JakeH678 <S0036442@wyke.ac.uk>
2025-03-28 10:32:20 +0000

    i cant remember

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let bg;
 let player;
 let enemy;
 let en;
+playerdead= false
+let score
+function scorecount(){
+    if (playerdead = false){
+        score +=1
+    }
+
+}
 function movement(){
     if (kb.pressing("shift")){
         if (kb.pressing("A")){
@@ -51,22 +59,36 @@ function preload(){
     bg = loadImage("assets/road.png")
 }
 
+let interavl1;
+
 
 function setup(){
 createCanvas(windowWidth,windowHeight)
 
+
+
 player = new Sprite(windowWidth/2,windowHeight-130,10,10,"d");
 player.img = "assets/car.png"
 player.scale = (windowWidth/800)/8
-player.rotation=270
-player.width = 120
-player.height = 120
-player.debug = true
+player.rotation = 270;
+player.width = 800*player.scale;
+player.height = 397*player.scale;
+player.debug = true;
 
 
 enemy = new Group();
 enemy.img = "assets/enemy.png"
-enemy.scale =0.025
+enemy.scale =(windowWidth/800)/8
+enemy.rotation= 270
+enemy.debug= true
+
+interavl1 = setInterval(() => {
+    if(enemy){
+            if(enemy.length < 6){
+                spawnenemy();
+            }
+    }
+}, 1000)
 }
 
 
@@ -74,9 +96,9 @@ function draw(){
 background(bg)
 movement()
 enemymovement()
-    while (enemy.length<enemynum){
-        let en = new enemy.Sprite(random(0,windowWidth),0,10,10,"d")
-    }
+enemycollide()
+textSize(30)
+text(score,windowWidth/2,20)
 }
 function enemymovement(){
     for(e of enemy){
@@ -84,5 +106,27 @@ function enemymovement(){
         if (e.y>windowHeight){
             e.remove()
         }
+    }
+}
+function enemycollide(){
+    for (e of enemy){
+        if (e.collides(player)){
+            e.remove()
+            endofgame()
+        }
+    }
+}
+function endofgame(){
+    for (e of enemy){
+        e.vel.y=0
+        playerdead = true
+    }
+}
+
+function spawnenemy(){
+    if(enemy.length<6){
+            let en = new enemy.Sprite(random(0,windowWidth),random(0,300),10,10,"d")
+            en.width=800*en.scale
+            en.height=400*en.scale
     }
 }
